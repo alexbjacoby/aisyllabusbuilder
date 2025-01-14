6b219ad0660fe694f34e20cba3d981176b438747
alexbjacoby <ajacoby@calarts.edu>
2025-01-14 13:56:38 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'; // <-- Must import the file containing @tailwind
 import AISyllabusStatementBuilder from './AISyllabusStatementBuilder';
 
 const rootElement = document.getElementById('root');
